122ec83117fc593192f7d618e77dca1c6446fcf9
Sebastian Menski <sebastian.menski@googlemail.com>
2014-12-05 15:52:30 +0100

    chore(prompt): fix bash prompt

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -1,77 +1,3 @@
-### define colors
-# Reset
-Color_Off='\e[0m'       # Text Reset
-
-# Regular Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
-
-# Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
-
-# Underline
-UBlack='\e[4;30m'       # Black
-URed='\e[4;31m'         # Red
-UGreen='\e[4;32m'       # Green
-UYellow='\e[4;33m'      # Yellow
-UBlue='\e[4;34m'        # Blue
-UPurple='\e[4;35m'      # Purple
-UCyan='\e[4;36m'        # Cyan
-UWhite='\e[4;37m'       # White
-
-# Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
-
-# High Intensity
-IBlack='\e[0;90m'       # Black
-IRed='\e[0;91m'         # Red
-IGreen='\e[0;92m'       # Green
-IYellow='\e[0;93m'      # Yellow
-IBlue='\e[0;94m'        # Blue
-IPurple='\e[0;95m'      # Purple
-ICyan='\e[0;96m'        # Cyan
-IWhite='\e[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\e[1;90m'      # Black
-BIRed='\e[1;91m'        # Red
-BIGreen='\e[1;92m'      # Green
-BIYellow='\e[1;93m'     # Yellow
-BIBlue='\e[1;94m'       # Blue
-BIPurple='\e[1;95m'     # Purple
-BICyan='\e[1;96m'       # Cyan
-BIWhite='\e[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\e[0;100m'   # Black
-On_IRed='\e[0;101m'     # Red
-On_IGreen='\e[0;102m'   # Green
-On_IYellow='\e[0;103m'  # Yellow
-On_IBlue='\e[0;104m'    # Blue
-On_IPurple='\e[0;105m'  # Purple
-On_ICyan='\e[0;106m'    # Cyan
-On_IWhite='\e[0;107m'   # White
-
 # configure git prompt
 GIT_PS1_SHOWDIRTYSTATE="Y"
 GIT_PS1_SHOWSTASHSTATE="Y"
@@ -79,13 +5,49 @@ GIT_PS1_SHOWUNTRACKEDFILES="Y"
 GIT_PS1_SHOWUPSTREAM="auto"
 
 DEFAULT_USER=menski
-PS_USER="$(if [ ${EUID} == 0 ]; then echo \\[$Red\\]\\u\\[$Color_Off\\]; elif [ \"$USER\" != \"$DEFAULT_USER\" ]; then echo \\u; fi)"
-PS_HOST="$(test -z "$TMUX" && echo \\h)"
-PS_AT="$(test -n "$PS_USER" -a -n "$PS_HOST" && echo @)"
-PS_DIV="$(test -n "$PS_USER" -o -n "$PS_HOST" && echo :)"
-PS_RC="\$(rc=\$?; test \$rc != 0 && echo \"\[$Red\]\$rc\[$Color_Off\] \")"
-PS_GIT="\$(__git_ps1 \" (%s)\")"
-PS1="${PS_USER}${PS_AT}${PS_HOST}${PS_DIV}\W${PS_GIT} ${PS_RC}\$ "
+
+function ps_root {
+  if [ $EUID -eq 0 ]; then
+    echo \\u
+  fi
+}
+
+function ps_user {
+  if [ -z "$(ps_root)" -a "$USER" != "$DEFAULT_USER" ]; then
+    echo \\u
+  fi
+}
+
+function ps_host {
+  if [ -z "$TMUX" -a "$TERM" != "screen" ]; then
+    echo \\h
+  fi
+}
+
+function ps_error {
+  local rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "$rc "
+  fi
+}
+
+function ps_delim1 {
+  if [ -n "$(ps_root)" -o -n "$(ps_user)" ]; then
+    if [ -n "$(ps_host)" ]; then
+      echo "@"
+    else
+      echo ":"
+    fi
+  fi
+}
+
+function ps_delim2 {
+  if [ -n "$(ps_host)" ]; then
+    echo ":"
+  fi
+}
+
+PS1="\[\e[;31m\]\$(ps_error)$(ps_root)\[\e[m\]$(ps_user)$(ps_delim1)$(ps_host)$(ps_delim2)\W\$(__git_ps1 \" (%s)\") $ "
 PS2="> "
 PS3="> "
 PS4="+ "
