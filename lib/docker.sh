c1b2dd104109ac68eda209a75fe86a4e31daf8a8
Sebastian Menski <sebastian.menski@googlemail.com>
2015-02-14 22:57:19 +0100

    feat(dmanage): add bash function to manage containers by files

--- lib/docker.sh
+++ lib/docker.sh
@@ -33,6 +33,74 @@ function dbash() {
 
 alias ddangling='docker rmi $(docker images -qf dangling=true)'
 
+function dmanage() {
+  local action=$1
+  local container=$2
+
+  if [ ! -f "$HOME/.docker/config/$container" ]; then
+    echo "Unknown container $container"
+    return 1
+  fi
+
+  . $HOME/.docker/config/$container
+
+  if [ -n "$RESTART" ]; then
+    RESTART="--restart $RESTART"
+  fi
+
+  case "$action" in
+    pull)
+      docker $DOCKER_OPTS pull $IMAGE
+      ;;
+    run)
+      docker $DOCKER_OPTS run -d -t --name $NAME $RESTART $OPTS $IMAGE
+      ;;
+    start)
+      docker $DOCKER_OPTS start $NAME
+      ;;
+    restart)
+      docker $DOCKER_OPTS restart $NAME
+      ;;
+    stop)
+      docker $DOCKER_OPTS stop $NAME
+      ;;
+    rmf)
+      docker $DOCKER_OPTS rm -f $NAME
+      ;;
+    rmi)
+      docker $DOCKER_OPTS rmi $IMAGE
+      ;;
+    logs)
+      docker $DOCKER_OPTS logs -f $NAME
+      ;;
+    reload)
+      dmanage rmf $container
+      dmanage run $container
+      ;;
+    update)
+      dmanage pull $container
+      dmanage reload $container
+      ;;
+    *)
+      echo "Unknown action $action"
+      return 2
+      ;;
+  esac
+}
+
+function _dmanage() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  if [ $COMP_CWORD -eq 1 ]; then
+    local options=( "pull run start restart stop rmf rmi logs reload update" )
+  else
+    IFS=$" "
+    local options=( $(/bin/ls $HOME/.docker/config/) )
+    unset IFS
+  fi
+  COMPREPLY=( $(compgen -W "${options[*]}" -- "$cur") )
+}
+
 if _is_bash; then
   complete -F __running_containers dip dssh dbash
+  complete -F _dmanage dmanage
 fi
